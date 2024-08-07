6f6247b19da72ae8d3390f5f066c87c481297e25
fevzi-kayaonu <fevzikayaonu35@gmail.com>
2024-08-07 20:31:56 +0300

    AddressContiner ve AddressForm tamamlandı

---
+++ src/components/form/AddressForm.jsx
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+
+export const AddressForm = () => {
+  const [formData, setFormData] = useState({
+    addressTitle: "",
+    fullName: "",
+    phone: "",
+    city: "",
+    district: "",
+    neighborhood: "",
+    addressDetails: "",
+  });
+
+  const cities = ["İstanbul", "Ankara", "İzmir", "Bursa", "Antalya"];
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
+  return (
+    <div className="fixed inset-0 bottom-44 top-24  max-w-lg mx-auto my-auto bg-white p-8 rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold mb-6">Adres Bilgisi Formu</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="addressTitle"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Adres Başlığı
+          </label>
+          <input
+            type="text"
+            id="addressTitle"
+            name="addressTitle"
+            value={formData.addressTitle}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Adres Başlığı"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="fullName"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Ad & Soyad
+          </label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Ad ve Soyad"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="phone"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Telefon
+          </label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Telefon"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="city"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Şehir (İl)
+          </label>
+          <select
+            id="city"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option value="">Şehir Seçiniz</option>
+            {cities.map((city, index) => (
+              <option key={index} value={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="district"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            İlçe
+          </label>
+          <input
+            type="text"
+            id="district"
+            name="district"
+            value={formData.district}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="İlçe"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="neighborhood"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Mahalle
+          </label>
+          <input
+            type="text"
+            id="neighborhood"
+            name="neighborhood"
+            value={formData.neighborhood}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Mahalle"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="addressDetails"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Adres
+          </label>
+          <textarea
+            id="addressDetails"
+            name="addressDetails"
+            value={formData.addressDetails}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Sokak, bina ve kapı numaraları"
+            rows="4"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Gönder
+        </button>
+      </form>
+    </div>
+  );
+};
--- src/components/order/AddressContainer.jsx
+++ src/components/order/AddressContainer.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import { AddressForm } from "../form/AddressForm";
 import { AddressCart } from "./AddressCart";
 
 export const AddressContainer = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+
+  const handleClose = () => {
+    setToggle(false);
+  };
+
   return (
     <div className="border-2 rounded-lg px-4 py-6">
       <div className="flex justify-between mb-8">
@@ -11,11 +23,11 @@ export const AddressContainer = () => {
         </h6>
       </div>
       <div className="flex gap-4 flex-wrap max-lg:flex-col">
-        <div className="flex justify-center items-center basis-[45%] m-auto aspect-[4/1] max-lg:aspect-[3/1]  max-md:w-[85%] border-2 rounded-lg p-4">
-          <div className="text-center">
-            <button className="text-5xl text-orange-500">+</button>{" "}
+        <div className="flex justify-center items-center basis-[45%] m-auto aspect-[4/1] max-lg:aspect-[3/1] max-md:w-[85%] border-2 rounded-lg p-4">
+          <div className="text-center" onClick={handleClick}>
+            <button className="text-5xl text-orange-500">+</button>
             <h6 className="text-base text-gray-500 font-bold">
-              Yeni Adres Ekle{" "}
+              Yeni Adres Ekle
             </h6>
           </div>
         </div>
@@ -25,6 +37,17 @@ export const AddressContainer = () => {
         <AddressCart />
         <AddressCart />
       </div>
+      {toggle && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div
+            className="absolute inset-0 bg-black opacity-50"
+            onClick={handleClose}
+          ></div>
+          <div className="relative bg-white p-8 rounded-lg shadow-md z-50">
+            <AddressForm />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
